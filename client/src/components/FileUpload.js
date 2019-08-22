d84d5b7165525393f1a805ec18aaa3b1e02c3f2a
chattavat.macOs <chattavat.v@gmail.com>
2019-08-22 22:53:54 +0700

    build message component

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,10 +1,12 @@
 import React, { Fragment, useState } from 'react';
+import Message from './Message';
 import axios from 'axios';
 
 const FileUpload = () => {
   const [file, setFile] = useState('');
   const [filename, setFilename] = useState('Choose File');
   const [uploadedFile, setUploadedFile] = useState({});
+  const [message, setMessage] = useState('');
 
   const onChange = e => {
     setFile(e.target.files[0]);
@@ -26,24 +28,30 @@ const FileUpload = () => {
       const { fileName, filePath } = res.data;
 
       setUploadedFile({ fileName, filePath });
+      setMessage('Upload File Complete');
     } catch (err) {
       if (err.response.status === 500) {
-        console.log('There was a problem with the server');
+        setMessage('There was a problem with the server');
       } else {
-        console.log(err.response.data.msg);
+        setMessage(err.response.data.msg);
       }
     }
   };
 
   return (
     <Fragment>
+      {
+        message ? <Message msg={message} /> : null
+      }
       <form onSubmit={onSubmit} >
         <div className="custom-file mb-4">
           <input type="file" className="custom-file-input" id="customFile" onChange={onChange}/>
           <label className="custom-file-label" htmlFor="customFile">{filename}</label>
         </div>
 
-        <input type="submit" value="Upload" className="btn btn-primary btn-block mt-4" />
+        <div className="col-3 mx-auto">
+          <input type="submit" value="Upload" className="btn btn-primary btn-block mt-4" />
+        </div>
       </form>
       {
         uploadedFile ? <div className="row mt-5">
